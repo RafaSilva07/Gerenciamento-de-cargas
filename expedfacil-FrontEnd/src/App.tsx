ec01575643038b726e5a517730d880e1222b8000
RafaelSilva <rafaelsilva.orizona@gmail.com>
2025-11-24 11:17:26 -0300

    :sparkles: Feat -> Adicionado pagina para criaçao do romaneio

--- expedfacil-FrontEnd/src/App.tsx
+++ expedfacil-FrontEnd/src/App.tsx
@@ -1,27 +1,38 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import HomePage from "./pages/home/HomePage";
+
 import ProdutosPage from "./pages/produtos/ProdutosPage";
+
 import RomaneiosPage from "./pages/romaneios/RomaneiosPage";
-import "./App.css";
 import VisualizarRomaneioPage from "./pages/romaneios/visualizarromaneio/VisualizarRomaneioPage";
+import NovoRomaneioPage from "./pages/romaneios/NovoRomaneioPage";
+
+import "./App.css";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
+
         {/* Página inicial */}
         <Route path="/" element={<HomePage />} />
 
-        {/* Gerenciamento de produtos */}
+        {/* Produtos */}
         <Route path="/produtos" element={<ProdutosPage />} />
 
-        {/* Gerenciamento de cargas (romaneios) */}
+        {/* Lista de Romaneios */}
         <Route path="/romaneios" element={<RomaneiosPage />} />
 
-        {/* Página de detalhes do romaneio (iremos criar depois) */}
-        <Route path="/romaneios/:numeroEmbarque" element={<VisualizarRomaneioPage />}
-/>
+        {/* Criar novo romaneio */}
+        <Route path="/romaneios/novo" element={<NovoRomaneioPage />} />
+
+        {/* Visualizar romaneio completo */}
+        <Route
+          path="/romaneios/:numeroEmbarque"
+          element={<VisualizarRomaneioPage />}
+        />
 
       </Routes>
     </Router>
